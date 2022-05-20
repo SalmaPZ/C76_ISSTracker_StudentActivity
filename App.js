452793993fb62dc94f7284bf121ff2d824597b3e
SalmaPZ <47847444+SalmaPZ@users.noreply.github.com>
2022-05-20 16:55:09 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,14 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import HomeScreen from "./screens/Home";
-import IssLocationScreen from "./screens/IssLocation";
-import MeteorScreen from "./screens/Meteors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> React Native!!</Text>
       <StatusBar style="auto" />
     </View>
   );
